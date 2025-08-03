2138e92c51e9317e3fed260665f0e54e2210c01c
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-08-03 21:53:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const stars = document.querySelectorAll(".star");
+const statusEl = document.querySelector(".status");
+const defaultRatingIndex = 0;
+let currentRatingIndex = 0;
+
+const checkSelectedStar = (star) => {
+  if (parseInt(star.getAttribute("data-rate")) === currentRatingIndex) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const setRating = (index) => {
+  stars.forEach((star) => star.classList.remove("selected"));
+  if (index > 0 && index <= stars.length) {
+    document
+      .querySelector('[data-rate="' + index + '"]')
+      .classList.add("selected");
+  }
+};
+
+const resetRating = () => {
+  currentRatingIndex = defaultRatingIndex;
+  setRating(defaultRatingIndex);
+};
+
+stars.forEach((star) => {
+  star.addEventListener("click", function () {
+    if (checkSelectedStar(star)) {
+      resetRating();
+      return;
+    }
+    const index = parseInt(star.getAttribute("data-rate"));
+    currentRatingIndex = index;
+    setRating(index);
+  });
+
+  star.addEventListener("mouseover", function () {
+    const index = parseInt(star.getAttribute("data-rate"));
+    setRating(index);
+  });
+
+  star.addEventListener("mouseout", function () {
+    setRating(currentRatingIndex);
+  });
+});
